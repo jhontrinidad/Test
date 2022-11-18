bfd5b678e4504431e54f27946240f445cdefc316
jhontrinidad <jh0ntrinidad24@gmail.com>
2022-11-18 20:16:45 +0800

    sample edit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -113,7 +113,7 @@ class ProductController extends Controller
 
         // if softDeletes is enabled
         // delete() = archieve
-        // forceDelete() = delete in db
+        // forceDelete() = delete in db hello
 
         // else
         // both will work
